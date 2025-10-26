ef0589c15c20af92d7e97ff6c252c574e4807a6d
mihakonceptcorn <skinzzadmin@gmail.com>
2025-10-26 21:04:37 +0200

    exercises

--- lib/data/substitutes.dart
+++ lib/data/substitutes.dart
@@ -2,11 +2,12 @@ import 'package:off_vape/models/substitute.dart';
 
 List<Substitute> substitutes = const [
   Substitute(title: '10 squats', imageSrc: 'assets/images/Squat.gif'),
-  Substitute(title: '10 push-ups', imageSrc: 'assets/images/Squat.gif'),
-  Substitute(title: '10 pull-ups', imageSrc: 'assets/images/Squat.gif'),
-  Substitute(title: '10 dips', imageSrc: 'assets/images/Squat.gif'),
-  Substitute(title: '1 minute plank', imageSrc: 'assets/images/Squat.gif'),
-  Substitute(title: '3 minutes of jump rope', imageSrc: 'assets/images/Squat.gif'),
-  Substitute(title: 'Deep breathing', imageSrc: 'assets/images/Squat.gif'),
-  Substitute(title: 'Other', imageSrc: 'assets/images/Squat.gif'),
+  Substitute(title: '10 push-ups', imageSrc: 'assets/images/PushUps.gif'),
+  Substitute(title: '10 pull-ups', imageSrc: 'assets/images/PullUps.gif'),
+  Substitute(title: '10 dips', imageSrc: 'assets/images/Dips.gif'),
+  Substitute(title: '1 minute plank', imageSrc: 'assets/images/Plank.gif'),
+  Substitute(title: '3 minutes of rope jumps', imageSrc: 'assets/images/Rope.gif'),
+  Substitute(title: '20 Burpee', imageSrc: 'assets/images/Burpee.gif'),
+  Substitute(title: '30 Jumping Jacks', imageSrc: 'assets/images/JumpingJack.gif'),
+  Substitute(title: 'Other', imageSrc: 'assets/images/Yoga.gif'),
 ];
